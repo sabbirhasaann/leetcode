a7ba322a2ff850ec8374dba4f1242999a6920783
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-05-21 00:38:13 +0600

    21/05/2025 daily problem currently not solved seeking solving techniques

---
+++ 3355/3355. Zero Array Transformation I.cpp
@@ -0,0 +1,20 @@
+// 2025/05/21 00:32:05
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+
+bool isZeroArray(vector<int> &nums, vector<vector<int>> &queries)
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    vector<int> nums;
+    vector<vector<int>> queries;
+}
