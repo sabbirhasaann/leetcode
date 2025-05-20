b168e3253a12738daaabea524296c29ad6d4eba8
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-05-21 00:45:27 +0600

    2179 problem with new branch

---
+++ 2179/2179. Count Good Triplets in an Array.cpp
@@ -0,0 +1,18 @@
+// 2025/04/15 09:12:56
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+long long goodTriplets(vector<int> &nums1, vector<int> &nums2)
+{
+    ll goodtriplets;
+    return goodtriplets;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
