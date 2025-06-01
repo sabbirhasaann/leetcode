41e0aac91c94243b01c4c7c55d21ba34638d0cbd
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-06-01 13:52:59 +0600

    added file cpp temp ,input images similar problems but problem is not solved now

---
+++ 3272/3272 Find the Count of Good Integers.cpp
@@ -0,0 +1,25 @@
+// 12/04/2025
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+long long countGoodIntegers(int n, int k)
+{
+    ll ans = 0;
+
+    return ans;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll n, k;
+    cin >> n >> k;
+
+    cout << countGoodIntegers(n, k) << endl;
+
+    return 0;
+}
