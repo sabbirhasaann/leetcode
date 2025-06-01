7a4447dfff0c6fcb26d25f985b8b05aff9dfa4a2
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-06-01 13:55:21 +0600

    2999 dont know problem is solved or not

---
+++ 2999/2999 Count the Number of Powerful Integers.cpp
@@ -0,0 +1,22 @@
+// 10-04-2025
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+
+long long numberOfPowerfulInt(long long start, long long finish, int limit, string s)
+{
+    return 123;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll start, finish;
+    int limit;
+    string s;
+    cout << "2999";
+}
