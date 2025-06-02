990bbecaaaa64279289e5ab510ebb2d8d3c15d20
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-06-02 15:54:54 +0600

    0135 some ideas are added cpp file added but problem is not solved

---
+++ 0135/0135 Candy.cpp
@@ -0,0 +1,13 @@
+// 2025/06/02 15:29:29
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
