98642c3e71d47e77063319fe63f3104bbff85a14
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-06-05 07:46:12 +0600

    1061 files are added

---
+++ 1061/1061. Lexicographically Smallest Equivalent String.cpp
@@ -0,0 +1,23 @@
+// 2025/06/05 07:41:24
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+
+string smallestEquivalentString(string s1, string s2, string baseStr)
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s1, s2, baseStr;
+    cin >> s1 >> s2 >> baseStr;
+    smallestEquivalentString(s1, s2, baseStr);
+
+    return 0;
+}
