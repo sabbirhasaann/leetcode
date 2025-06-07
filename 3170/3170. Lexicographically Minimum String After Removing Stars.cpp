d2ab89e172706707d6708a716883cdf37e181b8e
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-06-07 21:57:41 +0600

    3170 just file are added

---
+++ 3170/3170. Lexicographically Minimum String After Removing Stars.cpp
@@ -0,0 +1,20 @@
+// 2025/06/07 21:55:10
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+string clearStars(string s)
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    string s;
+    cin >> s;
+    cout << clearStars(s);
+    return 0;
+}
