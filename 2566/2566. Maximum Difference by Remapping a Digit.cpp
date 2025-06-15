e860afb48f197c1bac4e45a86fbdca2e68701019
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-06-15 12:02:34 +0600

    2566 just files are added

---
+++ 2566/2566. Maximum Difference by Remapping a Digit.cpp
@@ -0,0 +1,29 @@
+// 2025/06/14 22:59:51
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+int minMaxDifference(int num)
+{
+}
+void soln()
+{
+    int num;
+    cin >> num;
+    cout << minMaxDifference(num);
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        soln();
+    return 0;
+}
