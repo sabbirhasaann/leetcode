365060533123ea14e832c0599f9f830ae07ba54d
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-06-15 12:06:00 +0600

    1432 just files are added

---
+++ 1432/1432. Max Difference You Can Get From Changing an Integer.cpp
@@ -0,0 +1,30 @@
+// 2025/06/15 11:58:05
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+int maxDiff(int num)
+{
+}
+
+void soln()
+{
+    int num;
+    cin >> num;
+    cout << maxDiff(num);
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        soln();
+    return 0;
+}
