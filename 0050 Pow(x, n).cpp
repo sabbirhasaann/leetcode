79ef53eb8a399bdc409db0359fd301cc63b8b3f9
sabbirhasaann <sabbirhasan9989@gmail.com>
2025-12-18 17:06:29 +0600

    added others solutions

--- 0050 Pow(x, n).cpp
+++ 0050 Pow(x, n).cpp
@@ -24,6 +24,50 @@ double myPow(double x, int n)
         return 1 / ans;
     return ans;
 }
+
+double myPow2(double x, int n)
+{
+    if (n == 0)
+    {
+        return 1.0;
+    }
+    if (n > 0)
+    {
+        return 1.0 / myPow(x, -n);
+    }
+    if (n % 2)
+    {
+        return myPow(x, n + 1) / x;
+    }
+    double tmp = myPow(x, n / 2);
+    return tmp * tmp;
+}
+
+double power(double x, long long p)
+{
+    if (p == 0)
+        return 1;
+
+    double half = power(x, p / 2);
+
+    if (p % 2 == 0)
+        return half * half;
+    else
+        return half * half * x;
+}
+
+double myPow1(double x, int n)
+{
+    long long p = n; // handle INT_MIN safely
+
+    if (p < 0)
+    {
+        x = 1 / x;
+        p = -p;
+    }
+
+    return power(x, p);
+}
 void soln()
 {
     double x;
