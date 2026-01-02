d8cf0ce459c315763f5ae2db61daf276722de87f
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-01-02 14:54:45 +0600

    Others solution added for comparison

--- 0961 N-Repeated Element in Size 2N Array.cpp
+++ 0961 N-Repeated Element in Size 2N Array.cpp
@@ -17,8 +17,47 @@ int repeatedNTimes(vector<int> &nums)
             num = x.first;
             break;
         }
-        
+
     return num;
+}
+int repeatedNTimes3(vector<int> &nums)
+{
+    unordered_set<int> s;
+    for (int x : nums)
+    {
+        if (s.count(x))
+            return x;
+        s.insert(x);
+    }
+    return -1;
+}
+int repeatedNTimes2(vector<int> &nums)
+{
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        mp[nums[i]]++;
+
+        if (mp[nums[i]] > 1)
+        {
+            return nums[i];
+        }
+    }
+    return -1;
+}
+int repeatedNTimes1(vector<int> &nums)
+{
+
+    for (int i = 0; i < nums.size() - 2; i++)
+    {
+        if (nums[i] == nums[i + 1] || nums[i] == nums[i + 2])
+        {
+            return nums[i];
+        }
+    }
+
+    return nums[nums.size() - 1];
 }
 void soln()
 {
