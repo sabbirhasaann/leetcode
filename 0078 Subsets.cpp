14f70ee8119f3a63afe6e78fb06f4452c4e06162
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-01-15 19:57:49 +0600

    Solved 0078 Subsets beating runtime with 100% and memory with 42.01%'

---
+++ 0078 Subsets.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> res;
+    
+    for(int i=0;i< (1 << n);i++){
+        vector<int> sb;
+        for(int j=0;j<n;j++)
+        {
+            if(i & (1<<j))
+                sb.push_back(nums[j]);
+        }
+        res.push_back(sb);
+    }
+    
+    return res;
+}
+
+int main(){
+    vector<vector<int>> res;
+    vector<int> nums = {1,2,3};
+    res = subsets(nums);
+
+    for(int i=0;i<res.size();i++){
+        for(int j=0;j<res[i].size();j++)
+            cout << res[i][j] << " ";
+        cout << endl;
+    }
+    
+}
