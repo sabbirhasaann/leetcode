964154a8df96880c612bc54513dc3facefb8cb2f
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-01-24 16:27:29 +0600

    added other solution

--- 0009 Palindrome Number.cpp
+++ 0009 Palindrome Number.cpp
@@ -25,6 +25,24 @@ bool isPalindrome(int x)
 
     return true;
 }
+
+bool isPalindrome1(int x) {
+    // Negative numbers or numbers ending with 0 (except 0) are not palindromes
+    if (x < 0 || (x % 10 == 0 && x != 0))
+        return false;
+
+    int reversedHalf = 0;
+
+    // Reverse only half of the number
+    while (x > reversedHalf) {
+        reversedHalf = reversedHalf * 10 + x % 10;
+        x /= 10;
+    }
+
+    // For even digits: x == reversedHalf
+    // For odd digits: x == reversedHalf / 10
+    return (x == reversedHalf || x == reversedHalf / 10);
+}
 void soln()
 {
     int x = 121;
