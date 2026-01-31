558835be38ec0ba5870ab77bd04cbed7de9d40b2
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-01-31 19:09:42 +0600

    added others solution(soln)

--- 0744 Find Smallest Letter Greater Than Target.cpp
+++ 0744 Find Smallest Letter Greater Than Target.cpp
@@ -10,6 +10,35 @@ char nextGreatestLetter(vector<char>& letters, char target) {
             return ch;
     return letters[0]; 
 }
+
+char nextGreatestLetter2(vector<char>& letters, char target) {
+    int L = 0, R = letters.size() - 1;
+    while (L <= R) {
+        int M = (L + R) >> 1;
+        if (letters[M] > target) R = M - 1;
+        else L = M + 1;
+    }
+    return letters[L % letters.size()];
+}
+
+char nextGreatestLetter1(vector<char>& letters, char target) {
+    int beg = 0;
+    int end = letters.size() - 1;
+    char ans = letters[0];
+
+    while (beg <= end) {
+        int mid =beg+(end-beg)/ 2;
+
+        if (letters[mid] > target) {
+            ans = letters[mid];
+            end = mid - 1;
+        } else {
+            beg = mid + 1;
+        }
+    }
+    return ans;
+}
+
 void soln()
 {
     vector<char> letters = {'c','f','j'};
