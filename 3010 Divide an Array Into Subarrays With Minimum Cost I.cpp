aebd937ef42047e87452b369038808c907189a8b
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-01 12:58:29 +0600

    added other soln

--- 3010 Divide an Array Into Subarrays With Minimum Cost I.cpp
+++ 3010 Divide an Array Into Subarrays With Minimum Cost I.cpp
@@ -10,6 +10,32 @@ int minimumCost(vector<int>& nums) {
     return nums[0]+nums[1]+nums[2];
 }
 
+int minimumCost2(vector<int>& nums) {
+    int a = 51, b = 51, n = nums.size();
+    for(int i = 1; i < n; i++){
+        if(nums[i] < b){
+            b = nums[i];
+            if(b < a) swap(a, b);
+        }
+    }
+    return a + b + nums[0];
+}
+
+int minimumCost1(vector<int>& nums) {
+    int m1 = INT_MAX, m2 = INT_MAX;
+    for (int i = 1;i < nums.size();++i) {
+        int x = nums[i];
+        if (x < m1) {
+            m2 = m1;
+            m1 = x;
+        }
+        else if (x < m2) {
+            m2 = x;
+        }
+    }
+    return nums[0] + m1 + m2;
+}
+
 void soln()
 {
     vector<int> nums = {1,2,3,4,5};
