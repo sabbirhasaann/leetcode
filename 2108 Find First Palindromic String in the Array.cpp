f96bf5f1e7f93c14e86c2fba4ce080d1a18e6989
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-07 21:35:40 +0600

    Solved - beating runtime-21.61% memory-42.24%

---
+++ 2108 Find First Palindromic String in the Array.cpp
@@ -0,0 +1,39 @@
+// 2026/02/07 21:31:26
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+
+string firstPalindrome(vector<string>& words) {
+    for(string x: words){
+        bool flag = true;
+        for(int i=0,j=x.length()-1;i<j;++i,--j)
+        {
+            if(x[i]!=x[j]){
+                flag = false;
+                break;
+            }
+        }
+        if(flag)
+            return x;
+    }
+
+    return "";
+}
+
+void soln()
+{
+    vector<string> words = {"abc","car","ada","racecar","cool"};
+    cout << firstPalindrome(words) << endl;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    soln();
+    return 0;
+}
