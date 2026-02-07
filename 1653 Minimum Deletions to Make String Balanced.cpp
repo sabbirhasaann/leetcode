a978f5c2234f730008fd7f56d83297dc07d3e32a
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-07 21:14:33 +0600

    other soln

--- 1653 Minimum Deletions to Make String Balanced.cpp
+++ 1653 Minimum Deletions to Make String Balanced.cpp
@@ -19,6 +19,37 @@ int minimumDeletions(string s) {
     
     return res;    
 }
+
+int minimumDeletions1(string s) {
+    int res = 0, count = 0;
+    for (char c : s){
+        if (c == 'b')
+            count++;
+        else if (count){
+            res++;
+            count--;
+        }
+    }
+    return res;
+}
+
+int minimumDeletions2(string s) {
+    int bCount = 0;     // number of 'b' seen so far
+    int deletions = 0;  // minimum deletions
+
+    for(char c : s) {
+        if(c == 'b') {
+            bCount++;
+        } else { // c == 'a'
+            if(bCount > 0) {
+                deletions++;
+                bCount--;   // delete one previous 'b'
+            }
+        }
+    }
+    return deletions;
+}
+
 void soln()
 {
     string s = "baababbaabbaaabaabbabbbabaaaaaabaabababaaababbb";
