61015b81215a11b0e679db34ba1ce0d5303f0d1e
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-07 21:11:55 +0600

    07-02-2026 Daily Problem Solved - beating runtime - 91.27% memory - 72.34%

---
+++ 1653 Minimum Deletions to Make String Balanced.cpp
@@ -0,0 +1,42 @@
+// 2026/02/07 08:06:38
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+int minimumDeletions(string s) {
+    
+    int res = 0;
+    int countB = 0;
+    
+    for (char c : s) {
+        if (c == 'b') {
+            countB++;
+        } else {
+            res = min(res + 1, countB);
+        }
+    }
+    
+    return res;    
+}
+void soln()
+{
+    string s = "baababbaabbaaabaabbabbbabaaaaaabaabababaaababbb";
+    cout << minimumDeletions(s) << endl;
+    
+    s = "aababbab";
+    cout << minimumDeletions(s) << endl;
+    
+    s = "bbaaaaabb";    
+    cout << minimumDeletions(s) << endl;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    soln();
+    return 0;
+}
