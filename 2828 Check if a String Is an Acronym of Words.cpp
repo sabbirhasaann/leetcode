d325a789d82347b118f6aa9f5f812aa353e95280
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-07 21:29:25 +0600

    other soln

--- 2828 Check if a String Is an Acronym of Words.cpp
+++ 2828 Check if a String Is an Acronym of Words.cpp
@@ -12,6 +12,36 @@ bool isAcronym(vector<string>& words, string s) {
         return false;
     return true;
 }
+
+
+bool isAcronym1(vector<string>& words, string s) {
+    if (words.size() != s.size()) { return false; }
+    for (int i = 0; i < s.size(); ++i) {
+        if (!words[i].starts_with(s[i])) { return false; }
+    }
+    return true;
+}
+
+bool isAcronym2(vector<string>& words, string s) {
+    string newS="";
+
+    for(string w:words){
+        newS+=w[0];
+    }
+
+    return newS==s;
+}
+
+
+bool isAcronym3(vector<string>& words, string &s) {
+    int n = words.size();
+    if(s.size() != n) return false;
+    for(int i=0;i<n;i++) {
+        if(words[i][0] != s[i]) return false;
+    }
+    return true;
+}
+
 void soln()
 {
     vector<string> words = {"alice","bob","charlie"};
