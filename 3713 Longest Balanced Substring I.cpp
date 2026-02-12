14fa78c8d3c4a980400dd64aa72a5184cff2b400
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-12 11:35:45 +0600

    other soln

--- 3713 Longest Balanced Substring I.cpp
+++ 3713 Longest Balanced Substring I.cpp
@@ -37,6 +37,73 @@ int longestBalanced(string s) {
     }
     return max_ans ;
 }
+
+bool checkBalanced(vector<int> &freq){
+    int a = INT_MAX ;
+    for(int i =0;i<26;i++){
+        if(freq[i] != 0 ){
+            if(a== INT_MAX){
+                a= freq[i];
+            
+            }
+            else if(freq[i] != a){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+int longestBalanced1(string s) {
+    int n = s.size();
+    int maxlen = 1;
+    vector<int> letter_count(26,0);
+    for(int i = 0;i<n;i++){
+        letter_count.assign(26,0);
+        for(int j = i;j<n;j++){
+            letter_count[s[j] - 'a' ]++;
+            if(checkBalanced(letter_count)){
+                maxlen = max(maxlen,j-i+1);
+            }
+        }
+    }
+    return maxlen;
+}
+
+
+int longestBalanced2(string s) {
+    int n = s.size();
+
+    // Transform char -> int
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) 
+        a[i] = s[i] - 'a';
+
+    int result = 0;
+    for (int l = 0; l < n; ++l) {
+        // Early exit, can't be bigger
+        if (n - l <= result) break;
+
+        int cnt[26] = {0};          // Counts of every char
+        int uniq = 0, maxfreq = 0;  // Number of uniq chars and maximum frequency
+        for (int r = l; r < n; ++r) {
+            int i = a[r];
+
+            // There was no this char before => one more uniq
+            if (cnt[i] == 0) ++uniq;
+
+            ++cnt[i];
+            // Update max frequency
+            if (cnt[i] > maxfreq) 
+                maxfreq = cnt[i];
+
+            // Check if all uniq chars have maxfreq frequency then update the result
+            int cur = r - l + 1;
+            if (uniq * maxfreq == cur && cur > result)
+                result = cur;
+        }
+    }
+    return result;
+}
 void soln()
 {
     string s="abbac";
