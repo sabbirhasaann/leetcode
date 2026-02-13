e9ba43da48f41f592b3b3e09bbb210cc23d86292
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-13 21:53:16 +0600

    other soln

--- 0217 Contains Duplicate.cpp
+++ 0217 Contains Duplicate.cpp
@@ -16,7 +16,15 @@ bool containsDuplicate(vector<int>& nums) {
     return false;
 }
 
-
+bool containsDuplicate1(vector<int>& nums) {
+    int n=nums.size();
+    unordered_map<int,int>mpp;
+    for(int i=0;i<n;i++){
+        mpp[nums[i]]++;
+    }
+    if(mpp.size()==n)return false;
+    return true; 
+}
 void soln()
 {
     vector<int> nums = {1,2,3,1};
