172d68a5a9752e5c1d3062a60195f8cc496450bd
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-14 09:38:23 +0600

    other soln

--- 0799 Champagne Tower.cpp
+++ 0799 Champagne Tower.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #define ll long long
 #define endl '\n'
 void generator();
+double t[101][101]; 
 
 double champagneTower(int poured, int query_row, int query_glass) {
     
@@ -23,6 +24,64 @@ double champagneTower(int poured, int query_row, int query_glass) {
     }
 
     return min(1.0, tower[query_row][query_glass]);    
+}
+
+double solve(int poured, int row, int glass)
+{
+    
+    if(row < 0 || glass > row || glass < 0)
+    {
+        return 0.00;
+    }
+    
+    
+    if(row == 0 && glass == 0)
+    {
+        return poured;
+    }
+    
+    
+    if(t[row][glass] > -1)
+    {
+        return t[row][glass];
+    }
+    
+    
+    double left = (solve(poured, row - 1, glass - 1) - 1) / 2;
+    
+    
+    if(left < 0)
+    {
+        left = 0;
+    }
+    
+    double right = (solve(poured, row - 1, glass) - 1) / 2;
+    
+    
+    if(right < 0)
+    {
+        right = 0;
+    }
+    double total = left + right;
+    
+    return t[row][glass] = total;
+    
+    
+}
+double champagneTower1(int poured, int query_row, int query_glass) {
+    memset(t, -1, sizeof(t));
+    return min(1.00, solve(poured, query_row, query_glass));
+}
+
+double champagneTower2(int poured, int query_row, int query_glass) {
+    vector<double>dp(query_row+2,0.0);
+    dp[0]=poured;
+    for(int i=1;i<=query_row;i++){
+        for(int j=i;j>=0;j--){
+            dp[j+1] += dp[j] = max((dp[j]-1)/2,0.0);
+        }
+    }
+    return min(dp[query_glass],1.0);
 }
 
 void soln()
