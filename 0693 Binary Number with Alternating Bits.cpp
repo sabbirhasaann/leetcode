8691d9e6f0f58ae92fdc7bf011b0961773ef108f
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-18 22:06:55 +0600

    18-02-2026 Daily Problem Solved - beating runtime 100% and memory - 10.25%

---
+++ 0693 Binary Number with Alternating Bits.cpp
@@ -0,0 +1,63 @@
+// 2026/02/18 21:59:15
+// https://leetcode.com/problems/binary-number-with-alternating-bits/
+
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+#define ll long long
+#define endl '\n'
+bool hasAlternatingBits(int n) {
+        string s;
+        while(n){
+            if(n&1)
+                s+='1';
+            else
+                s+='0';
+            n>>=1;
+        }
+        reverse(s.begin(), s.end());
+
+        for(int i=0;i<s.length()-1;++i){
+            if(s[i]==s[i+1])
+                return false;
+        }
+        return true;
+    }
+void soln()
+{
+    auto start = high_resolution_clock::now();
+
+    int n=5;
+    hasAlternatingBits(n)? cout << "True\n" : cout << "False\n";
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Time taken: " << duration.count() << " ms" << endl;
+
+    start = high_resolution_clock::now();
+
+    n = 7;
+    hasAlternatingBits(n)? cout << "True\n" : cout << "False\n";
+
+    stop = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(stop-start);
+    cout << "Time taken: " << duration.count() << " ms" << endl;
+
+    start = high_resolution_clock::now();
+
+    n = 11;
+    hasAlternatingBits(n)? cout << "True\n" : cout << "False\n";
+
+    stop = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(stop-start);
+    cout << "Time taken: " << duration.count() << " ms" << endl;
+
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    soln();
+    return 0;
+}
