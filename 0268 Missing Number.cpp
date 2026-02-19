fbd38959d6e122eddebb840c51c65c09095b8968
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-19 16:13:56 +0600

    another soln

--- 0268 Missing Number.cpp
+++ 0268 Missing Number.cpp
@@ -16,6 +16,20 @@ int missingNumber(vector<int>& nums) {
         return missing;
 }
 
+int missingNumber1(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+
+        for(int i=1; i<=n; i++)
+            ans ^= i;
+
+        for(int i=0; i<nums.size(); i++){
+            ans ^= nums[i];
+        }
+
+        return ans;
+    }
+
 void soln()
 {
     vector<vector<int>> testSet{
