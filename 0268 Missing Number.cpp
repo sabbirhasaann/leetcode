f7f9c52ab038d76c2249dbfc4d7f90959f5974c1
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-19 15:55:28 +0600

    solved - beating runtime 100% memory 37.34%

---
+++ 0268 Missing Number.cpp
@@ -0,0 +1,42 @@
+// 2026/02/19 15:46:40
+// https://leetcode.com/problems/missing-number/
+
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+#define ll long long
+#define endl '\n'
+
+int missingNumber(vector<int>& nums) {
+        
+        int n = nums.size();
+        int missing = n * (n+1)/2;
+        for(int i=0;i<n;i++)
+            missing -= nums[i];
+        return missing;
+}
+
+void soln()
+{
+    vector<vector<int>> testSet{
+        {3,0,1},
+        {0,1},
+        {9,6,4,2,3,5,7,0,1}
+    };
+    for(int i=0;i<testSet.size();++i){
+        
+        auto start = high_resolution_clock::now();
+        cout << missingNumber(testSet[i])<< endl;
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<milliseconds>(stop - start);
+        cout << "Time taken: " << duration.count() << " ms" << endl;
+    }
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    soln();
+    return 0;
+}
