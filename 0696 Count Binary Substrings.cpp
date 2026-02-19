214ac98d90632d8af81592b719f7c303150d8916
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-19 19:28:49 +0600

    other soln

--- 0696 Count Binary Substrings.cpp
+++ 0696 Count Binary Substrings.cpp
@@ -27,6 +27,18 @@ int countBinarySubstrings(string s) {
     return ans;
 }
 
+int countBinarySubstrings(string s) {
+    int ans = 0, previous = 0, current = 1;
+    for(int i = 1 ; i < s.size() ; i++){
+        if(s[i] == s[i-1]) current++;
+        else{
+            previous = current;
+            current = 1;
+        }
+        if(current <= previous) ans++;
+    }
+    return ans;       
+}
 
 int countBinarySubstringsThought(string s) { // wa
         int ones = 0, zeros = 0, n = s.length(), cntsbstr = 0;
