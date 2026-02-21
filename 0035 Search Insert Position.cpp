8e1b0c679ccd67f60e4d9172183c0d3dd1fb6bea
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-21 15:31:06 +0600

    other soln

--- 0035 Search Insert Position.cpp
+++ 0035 Search Insert Position.cpp
@@ -23,6 +23,30 @@ int searchInsert(vector<int>& nums, int target) {
         return l;
     }
 
+int searchInsert1(vector<int>& nums, int target) {
+    int n= nums.size();
+    int s=0;
+    int end= n-1;
+    int ans=n;
+    while(s<=end){
+    int mid= s+(end-s)/2;
+    if(nums[mid]==target){
+        ans=mid;
+        break;
+    }
+    else if(nums[mid]<target){
+            s= mid+1;
+        
+    }
+    else{
+        ans= mid;
+        end=mid-1;
+    }
+
+    }
+    return ans; 
+}
+
 void soln()
 {
     vector<pair<vector<int>, int>> testSet{
