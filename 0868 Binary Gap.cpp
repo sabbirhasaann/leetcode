94ea2a7cbc4fc71fbdbedccb9ce4d5ab77b81387
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-22 20:50:44 +0600

    other soln

--- 0868 Binary Gap.cpp
+++ 0868 Binary Gap.cpp
@@ -36,6 +36,102 @@ int binaryGap(int n) {
         
 }
 
+int binaryGap1(int n) {
+
+        int gap = 0,b1=-1,b2=-1;
+        while (n)
+        {   
+            if(n&1){
+                if(b1!=-1){
+                    b1 = 1;
+                }
+
+            }
+
+            n>>=1;
+        }
+        
+        int len = s.size();
+        int k = 0;
+        for(int i=0;i<len;++i){
+            if(s[i]=='1'){
+                if(s[k]=='1'){
+                    gap = max(gap, i-k);
+                }
+                k = i;
+            }
+        }
+    
+        return gap;
+        
+}
+
+
+int binaryGap2(int n) {
+    n >>= __builtin_ctz(n);
+    if (n == 1) return 0;
+    int maxGap = 0, gap = 0;
+
+    while (n) {
+        if (n & 1) {
+            maxGap = max(maxGap, gap);
+            gap = 0;
+        } else
+            gap++;
+        n >>= 1;
+    }
+
+    return maxGap + 1;
+}
+
+int binaryGap3(unsigned n) {
+    int d=0, p=32;
+    for(; n>0; n&=(n-1)){
+        int ctz=countr_zero(n);
+        d=max(d, ctz-p);
+        p=ctz;
+    }
+    return d;
+}
+
+int binaryGap4(int n) {
+    int start=-1,end=-1;
+    int ans=0;
+
+    for(int i=0;i<32;i++){
+        int d=(n>>i) & 1;
+        if(d){
+            start=(start==-1)?i:start;
+            end=(end==-1)?start:i;
+            ans=max(ans,end-start);
+            start=end;
+        }
+    }
+
+    return ans;
+}
+
+
+int binaryGap5(int n) {   
+    int i=0;
+    int cnt = 0;
+    int idx = 0;
+    int gap = 0;
+    while(n!=0){
+        if((n&1)==1){                
+            if(cnt!=0){
+                gap = Math.max(gap, i-idx);                    
+            }
+            idx = i;
+            cnt++;
+        }
+        n=n>>1;
+        i++;
+    }
+
+    return gap;
+}
+
 void soln()
 {
     vector<int> testSet{
