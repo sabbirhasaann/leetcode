4c310a01f71b8bf1623fbd10900e1916808f0c99
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-26 23:07:14 +0600

    other soln

--- 0033 Search in Rotated Sorted Array.cpp
+++ 0033 Search in Rotated Sorted Array.cpp
@@ -50,6 +50,26 @@ int search(vector<int>& nums, int target) {
         return binary_search(nums, pivot + 1, n - 1, target);
     }
 
+int search1(vector<int>& nums, int target) {
+    int n = nums.size();
+    int left = 0, right = n - 1;
+    while (left < right) {
+        int mid = (left + right) >> 1;
+        if (nums[0] <= nums[mid]) {
+            if (nums[0] <= target && target <= nums[mid])
+                right = mid;
+            else
+                left = mid + 1;
+        } else {
+            if (nums[mid] < target && target <= nums[n - 1])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+    }
+    return nums[left] == target ? left : -1;
+}
+
 void soln()
 {
     vector<pair<vector<int>, int>> testSet{
