bb35e532526d82d9a8a1191fd40e486d644241ee
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-28 17:03:17 +0600

    other soln

--- 1680 Concatenation of Consecutive Binary Numbers.cpp
+++ 1680 Concatenation of Consecutive Binary Numbers.cpp
@@ -19,6 +19,54 @@ int concatenatedBinary(int n) {
         return ans;
     }
 
+int concatenatedBinary1(int n) 
+    {
+        const int MOD = 1e9 + 7;
+        long long ans = 0;
+        int bits = 0;
+        for (int i = 1; i <= n; i++) 
+        {
+            if ((i & (i - 1)) == 0) bits++;   
+            ans = ((ans << bits) % MOD + i) % MOD;
+        }
+        return ans;
+    }
+
+int concatenatedBinary2(int n) {
+        long long modulo = 1e9 + 7;
+        long long output = 0;
+        int len = 0;
+
+        for(int i = 1; i <=n; i++){
+            if((i & (i -1)) == 0 ) len++;
+
+            output <<= len;
+            output += i;
+            output %= modulo;
+        }
+
+        return output %= modulo;
+        
+    }
+
+int concatenatedBinary3(int n) {
+        long long ans = 0, len = 0, MOD = 1e9+7;
+        for(int i = 1; i <= n; i++){
+            if((i & (i-1)) == 0)len++;
+            ans = ((ans << len) | i) % MOD;
+        }
+        return ans;
+    }
+
+const int mod = 1e9 + 7;
+int concatenatedBinary4(int n) {
+    long result = 0;
+    for (int i = 1; i <= n; ++i) {
+        result = ((result << (std::bit_width((unsigned)i))) | i) % mod;
+    }
+    return result;
+}
+
 void soln()
 {
     vector<int> testSet{
