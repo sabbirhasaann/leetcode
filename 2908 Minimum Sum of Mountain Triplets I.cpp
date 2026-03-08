ccb0947748d4dbf1ae3170239493d85430744210
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-08 07:55:49 +0600

    more clean soln

--- 2908 Minimum Sum of Mountain Triplets I.cpp
+++ 2908 Minimum Sum of Mountain Triplets I.cpp
@@ -23,6 +23,33 @@ int minimumSum(vector<int>& nums) {
         return ans;
     }
 
+    int minimumSum1(vector<int>& nums) {
+        
+        int n = nums.size();
+        int ans = INT_MAX;
+        
+        for(int j = 1; j < n - 1; j++){
+            
+            int leftMin = INT_MAX;
+            int rightMin = INT_MAX;
+            
+            for(int i = 0; i < j; i++){
+                if(nums[i] < nums[j])
+                    leftMin = min(leftMin, nums[i]);
+            }
+            
+            for(int k = j + 1; k < n; k++){
+                if(nums[k] < nums[j])
+                    rightMin = min(rightMin, nums[k]);
+            }
+            
+            if(leftMin != INT_MAX && rightMin != INT_MAX)
+                ans = min(ans, leftMin + nums[j] + rightMin);
+        }
+        
+        return ans == INT_MAX ? -1 : ans;
+    }
+
 void soln()
 {
     vector<vector<int>> testSet{
