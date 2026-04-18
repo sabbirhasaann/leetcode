1d7720d37545817f4dd56e7177a626a4d717f6e6
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-04-18 08:10:30 +0600

    ac beating rnt-100% and mem-86%

---
+++ 3783 Mirror Distance of an Integer.cpp
@@ -0,0 +1,48 @@
+// 2026/04/18 08:05:13
+// https://leetcode.com/problems/mirror-distance-of-an-integer/
+
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+#define ll long long
+#define endl '\n'
+
+long long reverse(int x) {
+    
+    long long rev = 0;
+    while (x != 0) {
+        int digit = x % 10;
+        rev = rev * 10 + digit;
+        x /= 10;
+    }
+
+    return rev;        
+}
+
+int mirrorDistance(int n) {
+        long long rn = reverse(n);
+        return abs(rn-n);
+    }
+
+void soln()
+{
+    vector<int> testSet{
+        25,10,7
+    };
+    for(int i=0;i<testSet.size();++i){
+        
+        auto start = high_resolution_clock::now();
+        cout << mirrorDistance(testSet[i])<< endl;
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<milliseconds>(stop - start);
+        cout << "Time taken: " << duration.count() << " ms" << endl;
+    }
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    soln();
+    return 0;
+}
