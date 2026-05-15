fc471aa8723d417c8ef86c56a6eea099fcb6fd96
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-05-15 21:11:44 +0600

    other solution

--- 0153 Find Minimum in Rotated Sorted Array.cpp
+++ 0153 Find Minimum in Rotated Sorted Array.cpp
@@ -24,6 +24,29 @@ int findMin(vector<int> &nums)
     return nums[l];
 }
 
+int findMin1(vector<int> &nums)
+{
+    int n = nums.size();
+    int low = 0;
+    int high = n - 1;
+    int ans = INT_MAX;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (nums[low] <= nums[mid])
+        {
+            ans = min(ans, nums[low]);
+            low = mid + 1;
+        }
+        else
+        {
+            ans = min(ans, nums[mid]);
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
 void soln()
 {
     vector<vector<int>> testSet{
