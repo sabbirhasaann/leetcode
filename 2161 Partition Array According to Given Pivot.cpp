5f638bda0225fbd7af13dfcb9a5b07f6bcf6a2bc
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-09 06:22:21 +0600

    others solutions - 2161 Partition Array According to Given Pivot

--- 2161 Partition Array According to Given Pivot.cpp
+++ 2161 Partition Array According to Given Pivot.cpp
@@ -47,7 +47,140 @@ void soln()
         cout << "Time taken: " << duration.count() << " ms" << endl;
     }
 }
+vector<int> pivotArray1(vector<int> &nums, int pivot)
+{
+    vector<int> ans(nums.size());
+    int i = 0, j = 0, k = 0, c = 0;
+    while (i < nums.size())
+    {
+        if (nums[i] < pivot)
+        {
+            ans[k++] = nums[i];
+        }
+        else if (nums[i] == pivot)
+        {
+            c++;
+        }
+        i++;
+    }
+    for (int i = 0; i < c; i++)
+    {
+        ans[k++] = pivot;
+    }
+    while (j < nums.size())
+    {
+        if (nums[j] > pivot)
+        {
+            ans[k++] = nums[j];
+        }
+        j++;
+    }
+    return ans;
+}
+vector<int> pivotArray2(vector<int> &nums, int pivot)
+{
+    int n = nums.size();
+    int countLess = 0;
+    int countEqual = 0;
 
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] < pivot)
+        {
+            countLess++;
+        }
+        else if (nums[i] == pivot)
+        {
+            countEqual++;
+        }
+    }
+
+    int i = 0;
+    int j = countLess;
+    int k = countLess + countEqual;
+    vector<int> res(n);
+
+    for (int &num : nums)
+    {
+        if (num < pivot)
+        {
+            res[i] = num;
+            i++;
+        }
+        else if (num > pivot)
+        {
+            res[k] = num;
+            k++;
+        }
+        else
+        {
+            res[j] = num;
+            j++;
+        }
+    }
+    return res;
+}
+
+vector<int> pivotArray3(vector<int> &nums, int pivot)
+{
+    vector<int> result(nums.size(), 0);
+    int left = 0, right = nums.size() - 1;
+
+    for (int i = 0, j = nums.size() - 1; i < nums.size(); ++i, --j)
+    {
+        if (nums[i] < pivot)
+        {
+            result[left] = nums[i];
+            left++;
+        }
+
+        if (nums[j] > pivot)
+        {
+            result[right] = nums[j];
+            right--;
+        }
+    }
+
+    while (left <= right)
+    {
+        result[left] = pivot;
+        left++;
+    }
+
+    return result;
+}
+vector<int> pivotArray4(vector<int> &nums, int pivot)
+{
+    vector<int> result;
+    int count = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] == pivot)
+        {
+            count++;
+        }
+        if (nums[i] < pivot)
+        {
+            result.push_back(nums[i]);
+        }
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        result.push_back(pivot);
+    }
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] > pivot)
+        {
+            result.push_back(nums[i]);
+        }
+    }
+
+    return result;
+}
 int main()
 {
     ios_base::sync_with_stdio(false);
