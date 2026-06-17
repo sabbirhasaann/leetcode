88560781b1f8530d3235394bba3818f50fc82898
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-17 18:27:35 +0600

    another solution 3612 Process String with Special Operations I

--- 3612 Process String with Special Operations I.cpp
+++ 3612 Process String with Special Operations I.cpp
@@ -24,6 +24,23 @@ string processStr(string s)
     return r;
 }
 
+string processStr1(string s)
+{
+    string result = "";
+    for (auto it : s)
+    {
+        if (it >= 'a' && it <= 'z')
+            result += it;
+        else if (it == '*')
+            result = result.substr(0, result.size() - 1);
+        else if (it == '#')
+            result = result + result;
+        else
+            reverse(result.begin(), result.end());
+    }
+    return result;
+}
+
 void soln()
 {
     vector<string> testSet{
